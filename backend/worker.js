9802a73b47cf9d1e8832b3f317b95fe4350be93a
sidd1224 <siddhu12smp@gmail.com>
2025-12-15 17:22:34 +0530

    multiple votes handing done through caching

--- backend/worker.js
+++ backend/worker.js
@@ -16,7 +16,8 @@ let bufferTimer = null;
 
 const redisConnection = {
     host: process.env.REDIS_HOST || '127.0.0.1',
-    port: process.env.REDIS_PORT || 6379
+    port: process.env.REDIS_PORT || 6379,
+    password: process.env.REDIS_PASSWORD
 };
 
 // Initialize Worker
